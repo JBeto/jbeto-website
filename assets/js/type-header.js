d2d2b3ca838ac514eafc083b44004a913413898f
JBeto <j65beto@gmail.com>
2019-04-17 15:42:26 -0700

    Finished basic navigation bar

--- assets/js/type-header.js
+++ assets/js/type-header.js
@@ -1,6 +1,6 @@
 function typeSubtitle() {
     document.getElementById("typed").classList.remove("custom-cursor");
-    document.getElementById("typed-subtitle").classList.remove("hidden");
+    document.getElementById("typed-subtitle").classList.remove("is-invisible");
     var typed = new Typed('#typed-subtitle', {
         strings: ['^700 Software Developer'],
         typeSpeed: 65,
