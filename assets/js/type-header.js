d3834f001e08aa2e17eead49bdbac0f9296294b9
JBeto <j65beto@gmail.com>
2019-04-18 13:51:31 -0700

    Basic mobile hamburger toggle works, but needs mobile-specific styling

--- assets/js/type-header.js
+++ assets/js/type-header.js
@@ -1,5 +1,7 @@
 function typeSubtitle() {
+    // Remove custom cursor so it looks like that line is no longer typing
     document.getElementById("typed").classList.remove("custom-cursor");
+    // Next line begins typing
     document.getElementById("typed-subtitle").classList.remove("is-invisible");
     var typed = new Typed('#typed-subtitle', {
         strings: ['^700 Software Developer'],
